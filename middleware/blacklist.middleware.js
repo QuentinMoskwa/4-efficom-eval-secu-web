e36535084600f6fe03662d1be93c3761d6b6e034
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2025-05-07 11:42:22 +0200

    modif du blacklist.

--- middleware/blacklist.middleware.js
+++ middleware/blacklist.middleware.js
@@ -2,11 +2,9 @@ const { IpFilter } = require("express-ipfilter");
 const fs = require("fs");
 const path = require("path");
 
-// Charger les adresses IP depuis le fichier JSON
 const blacklistFilePath = path.join(__dirname, "../data/blacklist.json");
 const blacklistedIps = JSON.parse(fs.readFileSync(blacklistFilePath, "utf-8"));
 
-// Créer le middleware de filtrage
 const ipBlacklist = IpFilter(blacklistedIps, { mode: "deny" });
 
 module.exports = blacklist;
